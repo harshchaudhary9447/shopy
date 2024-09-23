8bc06721ca94386501650622a01bb6fb626b83ce
Harsh <harshchaudhary.work@gmail.com>
2024-09-23 22:55:40 +0530

    adding css

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,13 @@
+import { cn } from '@/lib/utils'
+import React, { ReactNode } from 'react'
+
+const MaxWidthWrapper = ({children,className}:{children:ReactNode 
+    className?:string}) => {
+  return (
+    <div className={cn('h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20 bg-black text-blue-700',className) }>
+        {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
